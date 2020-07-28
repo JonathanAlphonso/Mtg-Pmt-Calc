8a60a4408ce73ee6a3c84a58f10df6a8db5569b6
Jonathan Alphonso <zoltan3210@hotmail.com>
2020-07-28 14:51:38 -0400

    blank chart is generated

--- app.js
+++ app.js
@@ -47,30 +47,35 @@ var budgetController = (function () {
         },
 
         populateAmortTable: function (pmt, amortPeriod, payFreq, loanAmt, ratePerPmt) {
-            console.log(pmt, amortPeriod, payFreq, loanAmt, ratePerPmt);
-            console.log(loanAmt*ratePerPmt);
+           // console.log(pmt, amortPeriod, payFreq, loanAmt, ratePerPmt);
+           // console.log(loanAmt*ratePerPmt);
             var amortTable = {
                 id: [0],
                 pmt:[pmt.toFixed(2)],      
                 interest: [(loanAmt*ratePerPmt).toFixed(2)],
                 principal: [(pmt-loanAmt*ratePerPmt).toFixed(2)],
-                balance: [(loanAmt).toFixed(2)]
+                balance: [(loanAmt).toFixed(2)],
+                totalPeriods: [amortPeriod*payFreq]
             }
-            console.log(amortTable);
-            
-            for (var i = 1; i <= (amortPeriod*payFreq); i++) {
+            //console.log(amortTable);
+            var totalPeriods = amortPeriod*payFreq;
+            for (var i = 1; i <= totalPeriods; i++) {
                 amortTable.id[i] = i;
                 amortTable.pmt[i] = pmt.toFixed(2);
                 amortTable.interest[i] = (amortTable.balance[(i-1)]*ratePerPmt).toFixed(2);
-                console.log(amortTable.balance[(i-1)]);   
+                //console.log(amortTable.balance[(i-1)]);   
                 amortTable.principal[i] = (pmt-amortTable.interest[i]).toFixed(2);
                 amortTable.balance[i] = (amortTable.balance[i-1]-amortTable.principal[i]).toFixed(2);
                 if(amortTable.balance[i]<0){
-                    amortTable.principal[i] = parseFloat(amortTable.principal[i])+parseFloat(amortTable.balance[i]);
-                    amortTable.balance[i]=0;
+                    // amortTable.principal[i] = parseFloat(amortTable.principal[i])+parseFloat(amortTable.balance[i]);
+                    // amortTable.pmt[i] = parseFloat(amortTable.principal[i])+parseFloat(amortTable.interest[i]);
+                    // amortTable.balance[i]=0;
                 }
                 
             }
+            amortTable.principal[totalPeriods] = parseFloat(amortTable.principal[totalPeriods])+parseFloat(amortTable.balance[totalPeriods]);
+            amortTable.pmt[totalPeriods] = parseFloat(amortTable.principal[totalPeriods])+parseFloat(amortTable.interest[totalPeriods]);
+            amortTable.balance[totalPeriods]=0;
             //console.log(amortTable);
             
 
@@ -98,8 +103,8 @@ var UIController = (function () {
         getinput: function () {
             return {
                 //Convert from string with commas to number
-                mtgValue: (parseFloat(document.querySelector(DOMstrings.inputDebt).value.replace(/(?!\.)\D/g, '')) || 0),
-                interestRate: (parseFloat(document.querySelector(DOMstrings.inputRate).value.replace(/(?!\.)\D/g, '')) || 0),
+                mtgValue: (parseFloat(document.querySelector(DOMstrings.inputDebt).value.replace(/(?!\.)\D/g, ''))),
+                interestRate: (parseFloat(document.querySelector(DOMstrings.inputRate).value.replace(/(?!\.)\D/g, ''))),
                 amortPeriod: parseFloat(document.querySelector(DOMstrings.inputAmort).value),
                 payFreq: document.querySelector(DOMstrings.inputPayFreq).value
 
@@ -116,7 +121,7 @@ var UIController = (function () {
             if (!isNaN(pmt) && isFinite(pmt) && pmt > 0) {
                 newEl.setAttribute('class', 'LTV-Calc-Results flip-in-hor-bottom');
                 document.querySelector('#myChart').setAttribute('style', 'display:flex;')
-                newEl.innerHTML = `Your Payment is <strong>$ ${pmt}</strong>`;
+                newEl.innerHTML = `Your Payment is <strong>$ ${pmt.toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</strong>`;
             }
             else {
                 newEl.setAttribute('class', 'LTV-Calc-Results');
@@ -152,10 +157,10 @@ var UIController = (function () {
                 html += `
                         <tr>
                             <td>${tableData.id[i]}</td>
-                            <td>${tableData.pmt[i]}</td> 
-                            <td>${tableData.interest[i]}</td>
-                            <td>${tableData.principal[i]}</td>
-                            <td>${tableData.balance[i]}</td>
+                            <td>$${tableData.pmt[i].toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td> 
+                            <td>$${tableData.interest[i].toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
+                            <td>$${tableData.principal[i].toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
+                            <td>$${tableData.balance[i].toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
                         </tr>   
                         `                  
             }
@@ -174,52 +179,58 @@ var UIController = (function () {
             return DOMstrings;
         },
 
-        addChart: function (ltv) {
-            var data = {
-                labels: [
-                    "Mortgage Debt",
-                    "Home Equity"
-                ],
-                datasets: [
-                    {
-                        data: [ltv, 100 - ltv],
-                        backgroundColor: [
-                            "#FF6384",
-                            "#36A2EB"
+        addChart: function (numOfPayment,balance) {
+            var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            var config = {
+                type: 'line',
+                data: {
+                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                    datasets: [{
+                        label: 'My First dataset',
+           
+                        data: [
+                           1,2,3,4,5
                         ],
-                        hoverBackgroundColor: [
-                            "#FF6384",
-                            "#36A2EB",
-                        ]
+                        fill: false,
                     }]
+                },
+                options: {
+                    responsive: true,
+                    title: {
+                        display: true,
+                        text: 'Chart.js Line Chart'
+                    },
+                    tooltips: {
+                        mode: 'index',
+                        intersect: false,
+                    },
+                    hover: {
+                        mode: 'nearest',
+                        intersect: true
+                    },
+                    scales: {
+                        xAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Month'
+                            }
+                        }],
+                        yAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Value'
+                            }
+                        }]
+                    }
+                }
             };
 
             var ctx = document.getElementById("myChart");
 
             // And for a doughnut chart
-            var myDoughnutChart = new Chart(ctx, {
-                type: 'doughnut',
-                data: data,
-                options: {
-                    rotation: 1 * Math.PI,
-                    circumference: 1 * Math.PI,
-                    tooltips: {
-                        callbacks: {
-                            label: function (tooltipItem, data) {
-                                var dataset = data.datasets[tooltipItem.datasetIndex];
-                                var meta = dataset._meta[Object.keys(dataset._meta)[0]];
-                                var total = meta.total;
-                                var currentValue = dataset.data[tooltipItem.index];
-                                var percentage = parseFloat((currentValue / total * 100).toFixed(1));
-                                return percentage + '%';
-                            },
-                            title: function (tooltipItem, data) {
-                                return data.labels[tooltipItem[0].index];
-                            }
-                        }
-                    }
-                }
-            });
+            var myDoughnutChart = new Chart(ctx, config);
         }
     };
 })();
@@ -268,10 +279,11 @@ var controller = (function (budgetCtrl, UICtrl) {
         //UICtrl.addLtv(newItem);
         UICtrl.addPmt(newItem);
         //Add the cool chart js chart
-        //UICtrl.addChart(newItem);
+        
         
         tableData = budgetCtrl.populateAmortTable(newItem, input.amortPeriod, parseFloat(input.payFreq), input.mtgValue,ratePerPayment);
         UICtrl.addAmortTable(tableData);
+        UICtrl.addChart(tableData.balance);
     };
 
     return {
